29b3d844197a0a243ac4fabd44e81c7171475ce1
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2023-09-08 15:26:10 +0300

    just cleaning code

--- public/dashboard-script.js
+++ public/dashboard-script.js
@@ -1,83 +1,3 @@
-// // Get references to HTML elements
-
-// const matchesContainer = document.getElementById("laliga");
-// const addMatchForm = document.getElementById("add-match-form");
-// const team1Input = document.getElementById("team1");
-// const team2Input = document.getElementById("team2");
-
-// // Event handler for adding a match
-// addMatchForm.addEventListener("submit", function (event) {
-//   event.preventDefault();
-
-//   // Get the input values
-//   const team1 = team1Input.value;
-//   const team2 = team2Input.value;
-
-//   // Create a new match element
-//   const matchElement = document.createElement("div");
-//   matchElement.className = "match";
-//   matchElement.innerHTML = `
-//   <div class="l-logo">
-//       <img src="/Logos/Laliga/newLiga.ico" alt="laliga" />
-//   </div>
-//   <div class="home">
-//       <div class="team-name">
-//           <p>${team1}</p>
-//       </div>
-//       <div class="team">
-//           <img src="/Logos/Laliga/Teams/${team1.toLowerCase()}.png" alt="${team1}" />
-//       </div>
-//   </div>
-//   <div class="vs">
-//       <p>VS</p>
-//   </div>
-//   <div class="away">
-//       <div class="team">
-//           <img src="/Logos/Laliga/Teams/${team2.toLowerCase()}.png" alt="${team2}" />
-//       </div>
-//       <div class="team-name">
-//           <p>${team2}</p>
-//       </div>
-//   </div>
-
-// `;
-
-//   // Append the match element to the container
-//   matchesContainer.appendChild(matchElement);
-
-//   // Clear the input fields
-//   team1Input.value = "";
-//   team2Input.value = "";
-// });
-/////////////////////////////////////////////////////////////////////////////////////////////
-// Remove the module.exports from this file, as it's not needed for this task.
-// const addMatchForm = document.getElementById("add-match-form");
-// const team1Input = document.getElementById("team1");
-// const team2Input = document.getElementById("team2");
-
-// // Event handler for adding a match
-// // Event handler for adding a match
-// addMatchForm.addEventListener("submit", function (event) {
-//   event.preventDefault();
-
-//   // Get the input values
-//   const team1 = team1Input.value;
-//   const team2 = team2Input.value;
-
-//   // Retrieve existing matches from localStorage or initialize an empty array
-//   const existingMatches = JSON.parse(localStorage.getItem("matches")) || [];
-
-//   // Add the new match to the array
-//   existingMatches.push({ team1, team2 });
-
-//   // Store the updated array in localStorage
-//   localStorage.setItem("matches", JSON.stringify(existingMatches));
-
-//   // Clear the input fields
-//   team1Input.value = "";
-//   team2Input.value = "";
-// });
-
 const addllMatch = document.getElementById("add-ll-match");
 const addPLMatch = document.getElementById("add-pl-match"); // Add PL match form
 const addSAMatch = document.getElementById("add-seriea-match"); // Add SA match form
@@ -92,6 +12,18 @@ const deletePLMatchButton = document.getElementById("delete-pl-match-button"); /
 const deleteSAMatchButton = document.getElementById("delete-sa-match-button"); // Delete SA match button
 
 //////////     LALIGA CODE
+
+//Function to add laliga matches to localstorage
+function addllMatchToLocal(team1, team2) {
+  // Retrieve existing matches from localStorage or initialize an empty array
+  const llexistingMatches = JSON.parse(localStorage.getItem("matches")) || [];
+
+  // Add the new match to the array
+  llexistingMatches.push({ team1, team2 });
+
+  // Store the updated array in localStorage
+  localStorage.setItem("matches", JSON.stringify(llexistingMatches));
+}
 
 // Function to delete a match from localStorage laliga
 function deleteMatchFromLocal(team1, team2) {
@@ -129,14 +61,7 @@ addllMatch.addEventListener("submit", function (event) {
   const team1 = team1Input.value;
   const team2 = team2Input.value;
 
-  // Retrieve existing matches from localStorage or initialize an empty array
-  const llexistingMatches = JSON.parse(localStorage.getItem("matches")) || [];
-
-  // Add the new match to the array
-  llexistingMatches.push({ team1, team2 });
-
-  // Store the updated array in localStorage
-  localStorage.setItem("matches", JSON.stringify(llexistingMatches));
+  addllMatchToLocal(team1, team2);
 
   // Clear the input fields
   team1Input.value = "";
@@ -204,6 +129,8 @@ deletePLMatchButton.addEventListener("click", function () {
   plTeam1Input.value = "";
   plTeam2Input.value = "";
 });
+
+
 
 ///////   SERIE A CODE
 
