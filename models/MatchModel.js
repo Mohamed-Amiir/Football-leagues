1cd126b8b91deb993ae5a2f8f3989a48ad331270
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2023-09-09 20:23:48 +0300

    Implement Match functionality with JSON

--- models/MatchModel.js
+++ models/MatchModel.js
@@ -1,26 +1,47 @@
-// models/MatchModel.js
+const fs = require("fs");
+const path = require("path");
+const matchsPath = path.join(__dirname, "../matchs.json");
 
-const mongoose = require("mongoose");
+module.exports = class Match {
+  constructor(t1, t2) {
+    this.team1 = t1;
+    this.team2 = t2;
+  }
 
-// Define a schema for the "laligas" collection
-const teamSchema = new mongoose.Schema({
-  name: String,
-  logo: Buffer,
-});
+  saveMatch() {
+    // 1) read from file
+    fs.readFile(matchsPath, (err, info) => {
+      if (!err) {
+        let Matchs = JSON.parse(info);
 
-// Define the "Match" schema that references the "laligas" collection
-const matchSchema = new mongoose.Schema({
-  homeTeam: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "laligas", // Reference the "laligas" collection
-  },
-  awayTeam: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "laligas", // Reference the "laligas" collection
-  },
-});
+        // 2) update data
+        Matchs.push({
+          team1: this.team1,
+          team2: this.team2,
+        });
 
-module.exports = {
-  Match: mongoose.model("Match", matchSchema),
-  Team: mongoose.model("laligas", teamSchema), // Export the "laligas" model
+        // 3) write into file
+        fs.writeFile(matchsPath, JSON.stringify(Matchs), (err) => {
+          if (err) {
+            console.log("Error occurred while writing to file:", err);
+          } else {
+            console.log("Data saved successfully.");
+          }
+        });
+      } else {
+        console.log("Error occurred while reading file:", err);
+      }
+    });
+  }
+
+  static fetchAllStudents(callback) {
+    fs.readFile(matchsPath, (err, info) => {
+      if (!err) {
+        callback(JSON.parse(info));
+      } else {
+        console.log("Error occurred while reading file:", err);
+        callback([]);
+      }
+    });
+  }
 };
