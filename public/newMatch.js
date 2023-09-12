e56a627cd282455019e8880c12fb06966e82437c
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2023-09-12 17:33:01 +0300

    Setting the whole app with JSON -NO VALIDATION-

--- public/newMatch.js
+++ public/newMatch.js
@@ -129,6 +129,8 @@
 
 // Retrieve the existing matches for LaLiga from localStorage
 const matchesContainer = document.getElementById("laliga");
+const plMatchesContainer = document.getElementById("pl"); // Premier League Matchs Container
+const saMatchesContainer = document.getElementById("seriea"); // Sirea A Matchs Container
 
 // Function to create a match element for LaLiga
 function createLaLigaMatchElement(team1, team2) {
@@ -163,7 +165,7 @@ function createLaLigaMatchElement(team1, team2) {
   return matchElement;
 }
 function fetchMatchs() {
-  fetch("/dashboard/fetch")
+  fetch("/dashboard/fetch/Laliga")
     .then((response) => response.json())
     .then((matches) => {
       matchesContainer.innerHTML = "";
@@ -176,3 +178,99 @@ function fetchMatchs() {
 }
 
 fetchMatchs();
+
+/////////////////////////////////////////////////////////////////////////////
+function createPremierLeagueMatchElement(team1, team2) {
+  const matchElement = document.createElement("div");
+  matchElement.className = "match"; // Add a class for LaLiga matches
+  matchElement.innerHTML = `
+      <!-- Your match HTML template here -->
+      <div class="l-logo">
+          <img src="/Logos/Premier-League/pl.jpeg" alt="Premier-League" />
+      </div>
+      <div class="home">
+          <div class="team-name">
+              <p>${team1.name}</p>
+          </div>
+          <div class="team">
+              <img src="${team1.logo}" alt="${team1.name}" />
+          </div>
+      </div>
+      <div class="vs">
+          <p>VS</p>
+      </div>
+      <div class="away">
+          <div class="team">
+              <img src="${team2.logo}" alt="${team2.name}" />
+          </div>
+          <div class="team-name">
+              <p>${team2.name}</p>
+          </div>
+      </div>
+  `;
+
+  return matchElement;
+}
+function fetchPLMatchs() {
+  fetch("/dashboard/fetch/PremierLeague")
+    .then((response) => response.json())
+    .then((matches) => {
+      plMatchesContainer.innerHTML = "";
+      matches.forEach((match) => {
+        const mtch = createPremierLeagueMatchElement(
+          match.teamOne,
+          match.teamTwo
+        ); // Corrected typo
+        plMatchesContainer.appendChild(mtch);
+      });
+    })
+    .catch((error) => console.error(error));
+}
+fetchPLMatchs();
+/////////////////////////////////////////////////////////////////////////////////
+
+function createSireaAMatchElement(team1, team2) {
+  const matchElement = document.createElement("div");
+  matchElement.className = "match"; // Add a class for LaLiga matches
+  matchElement.innerHTML = `
+      <!-- Your match HTML template here -->
+      <div class="l-logo">
+          <img src="/Logos/SireaA/seriea.jpg" alt="Sirea A" />
+      </div>
+      <div class="home">
+          <div class="team-name">
+              <p>${team1.name}</p>
+          </div>
+          <div class="team">
+              <img src="${team1.logo}" alt="${team1.name}" />
+          </div>
+      </div>
+      <div class="vs">
+          <p>VS</p>
+      </div>
+      <div class="away">
+          <div class="team">
+              <img src="${team2.logo}" alt="${team2.name}" />
+          </div>
+          <div class="team-name">
+              <p>${team2.name}</p>
+          </div>
+      </div>
+  `;
+
+  return matchElement;
+}
+function fetchSAMatchs() {
+  fetch("/dashboard/fetch/SireaA")
+    .then((response) => response.json())
+    .then((matches) => {
+      saMatchesContainer.innerHTML = "";
+      matches.forEach((match) => {
+        const mtch = createSireaAMatchElement(match.teamOne, match.teamTwo); // Corrected typo
+        saMatchesContainer.appendChild(mtch);
+      });
+    })
+    .catch((error) => console.error(error));
+}
+
+fetchSAMatchs();
