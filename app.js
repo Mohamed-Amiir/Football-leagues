f3e0d927dac9f67991deef8cf0462a4ad986d04c
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2023-09-13 21:30:22 +0300

    MVC

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 8001;
+const port = process.env.PORT || 3111;
 const bodyParser = require("body-parser");
 const matchRoutes = require("./routes/matchRoutes");
 app.use(bodyParser.json());
 // 3rd party middleware
 app.use(bodyParser.urlencoded({ extended: true })); // Use bodyParser for parsing form data
-app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/dashboard", matchRoutes);
 
 // Request main
 app.get("/dashboard", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../Football-leagues/public/dashboard.html")
-  );
+  res.sendFile(path.join(__dirname, "./public/Dashboard/dashboard.html"));
 });
-app.get("/football", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../Football-leagues/public/football.html")
-  );
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/Main-Page/football.html"));
 });
 
 app.listen(port, () => {
