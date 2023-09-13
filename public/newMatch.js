0e5cb4e27c4480f2af37ff767aa42d394559131d
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2023-09-13 11:41:31 +0300

    Delet useless code

--- public/newMatch.js
+++ public/newMatch.js
@@ -1,138 +1,9 @@
-// // Retrieve the existing matches for LaLiga from localStorage
-// const existingLLMatches = JSON.parse(localStorage.getItem("matches")) || [];
-// const matchesContainer = document.getElementById("laliga");
-
-// // Retrieve the existing matches for PL from localStorage
-// const existingPLMatches = JSON.parse(localStorage.getItem("plMatches")) || [];
-// const plMatchesContainer = document.getElementById("pl"); // PL container
-
-// const existingSAMatches = JSON.parse(localStorage.getItem("saMatches")) || [];
-// const saMatchesContainer = document.getElementById("seriea"); // PL container
-
-// // Function to create a match element for LaLiga
-// function createLaLigaMatchElement(team1, team2) {
-//   const matchElement = document.createElement("div");
-//   matchElement.className = "match"; // Add a class for LaLiga matches
-//   matchElement.innerHTML = `
-//       <!-- Your match HTML template here -->
-//       <div class="l-logo">
-//           <img src="/Logos/Laliga/newLiga.ico" alt="LaLiga" />
-//       </div>
-//       <div class="home">
-//           <div class="team-name">
-//               <p>${team1}</p>
-//           </div>
-//           <div class="team">
-//               <img src="/Logos/Laliga/Teams/${team1.toLowerCase()}.png" alt="${team1}" />
-//           </div>
-//       </div>
-//       <div class="vs">
-//           <p>VS</p>
-//       </div>
-//       <div class="away">
-//           <div class="team">
-//               <img src="/Logos/Laliga/Teams/${team2.toLowerCase()}.png" alt="${team2}" />
-//           </div>
-//           <div class="team-name">
-//               <p>${team2}</p>
-//           </div>
-//       </div>
-//   `;
-
-//   return matchElement;
-// }
-// // Function to create a match element for Premier League
-// function createPLMatchElement(team1, team2) {
-//   const matchElement = document.createElement("div");
-//   matchElement.className = "match"; // Add a class for PL matches
-//   matchElement.innerHTML = `
-//       <!-- Your match HTML template here -->
-//       <div class="l-logo">
-//           <img src="/Logos/Premier-League/pl.jpeg" alt="Premier League" />
-//       </div>
-//       <div class="home">
-//           <div class="team-name">
-//               <p>${team1}</p>
-//           </div>
-//           <div class="team-pl">
-//               <img src="/Logos/Premier-League/Teams/${team1.toLowerCase()}.svg" alt="${team1}" />
-//           </div>
-//       </div>
-//       <div class="vs">
-//           <p>VS</p>
-//       </div>
-//       <div class="away">
-//           <div class="team-pl">
-//               <img src="/Logos/Premier-League/Teams/${team2.toLowerCase()}.svg" alt="${team2}" />
-//           </div>
-//           <div class="team-name">
-//               <p>${team2}</p>
-//           </div>
-//       </div>
-//   `;
-
-//   return matchElement;
-// }
-// // Function to create a match element for Sirea A
-// function createSAMatchElement(team1, team2) {
-//   const matchElement = document.createElement("div");
-//   matchElement.className = "match"; // Add a class for SA matches
-//   matchElement.innerHTML = `
-//       <!-- Your match HTML template here -->
-//       <div class="l-logo">
-//           <img src="/Logos/SireaA/seriea.jpg" alt="Sirea A" />
-//       </div>
-//       <div class="home">
-//           <div class="team-name">
-//               <p>${team1}</p>
-//           </div>
-//           <div class="team-sa">
-//               <img src="/Logos/SireaA/Teams/${team1.toLowerCase()}.webp" alt="${team1}" />
-//           </div>
-//       </div>
-//       <div class="vs">
-//           <p>VS</p>
-//       </div>
-//       <div class="away">
-//           <div class="team-sa">
-//               <img src="/Logos/SireaA/Teams/${team2.toLowerCase()}.webp" alt="${team2}" />
-//           </div>
-//           <div class="team-name">
-//               <p>${team2}</p>
-//           </div>
-//       </div>
-//   `;
-
-//   return matchElement;
-// }
-
-// // Iterate through the existing LaLiga matches and create match elements
-// existingLLMatches.forEach((matchData) => {
-//   const { team1, team2 } = matchData;
-//   const matchElement = createLaLigaMatchElement(team1, team2);
-//   matchesContainer.appendChild(matchElement);
-// });
-
-// // Iterate through the existing PL matches and create match elements
-// existingPLMatches.forEach((matchData) => {
-//   const { team1, team2 } = matchData;
-//   const matchElement = createPLMatchElement(team1, team2);
-//   plMatchesContainer.appendChild(matchElement);
-// });
-
-// // Iterate through the existing SA matches and create match elements
-// existingSAMatches.forEach((matchData) => {
-//   const { team1, team2 } = matchData;
-//   const matchElement = createSAMatchElement(team1, team2);
-//   saMatchesContainer.appendChild(matchElement);
-// });
-
-// Retrieve the existing matches for LaLiga from localStorage
-const matchesContainer = document.getElementById("laliga");
+const matchesContainer = document.getElementById("laliga"); // Laliga Matchs Container
 const plMatchesContainer = document.getElementById("pl"); // Premier League Matchs Container
 const saMatchesContainer = document.getElementById("seriea"); // Sirea A Matchs Container
 
-// Function to create a match element for LaLiga
+//////////////////////////////////////      LALIGA       //////////////////////////////////////
+
 function createLaLigaMatchElement(team1, team2) {
   const matchElement = document.createElement("div");
   matchElement.className = "match"; // Add a class for LaLiga matches
@@ -176,10 +47,10 @@ function fetchMatchs() {
     })
     .catch((error) => console.error(error));
 }
-
 fetchMatchs();
 
-/////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////      PREMIER LEAGUE       //////////////////////////////////////
+
 function createPremierLeagueMatchElement(team1, team2) {
   const matchElement = document.createElement("div");
   matchElement.className = "match"; // Add a class for LaLiga matches
@@ -227,7 +98,8 @@ function fetchPLMatchs() {
     .catch((error) => console.error(error));
 }
 fetchPLMatchs();
-/////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////      SIREA A       //////////////////////////////////////
 
 function createSireaAMatchElement(team1, team2) {
   const matchElement = document.createElement("div");
@@ -272,5 +144,4 @@ function fetchSAMatchs() {
     })
     .catch((error) => console.error(error));
 }
-
 fetchSAMatchs();
--- routes/matchRoutes.js
+++ routes/matchRoutes.js
@@ -1,14 +1,9 @@
-// matchRoutes.js
-
 const express = require("express");
 const router = express.Router();
 const MatchController = require("../controllers/MatchesController");
 
-// Define a route to create a new match
-router.post("/:league", MatchController.insertNewMatch);
 router.get("/fetch/:league", MatchController.getAllMatchs);
+router.post("/:league", MatchController.insertNewMatch);
 router.delete("/:league/:team1", MatchController.deleteMatch);
-
-// Define other routes for fetching matches, updating, deleting, etc. if needed
 
 module.exports = router;
