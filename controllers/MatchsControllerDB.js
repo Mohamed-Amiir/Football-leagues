c83b11aaf32903c1b3735d72296534b9b589035b
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2023-09-19 16:24:13 +0300

    Final Project -MongoDB-

--- controllers/MatchsControllerDB.js
+++ controllers/MatchsControllerDB.js
@@ -1,21 +1,7 @@
 const Match = require("../models/MatchModelDB");
 
-let getAllMatchs = async (req, res) => {
-  const LEAGUE = req.params.league;
-  let matchs = await Match.LEAGUE.findOne({}).matchs;
-  res.send(matchs);
-  console.log("Display all students informations....");
-};
-
-let insertNewMatch = (req, res) => {
-  const LEAGUE = req.params.league;
-  let mtch = new Match.LEAGUE({
-    team1: req.body.team1,
-    team2: req.body.team2,
-  });
-  Match.LEAGUE.findOne({}).teams.push(mtch);
-};
-
+let getAllMatchs = async (req, res) => {};
+let insertNewMatch = (req, res) => {};
 let deleteMatch = (req, res) => {};
 
 module.exports = {
